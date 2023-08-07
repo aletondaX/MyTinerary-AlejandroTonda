8cf92b872999cede5506cdd2776dfdac8ab6ddda
aletondaX <gtrrsta@outlook.com>
2023-08-07 15:54:19 -0300

    Improved carousel slideshow - Started responsive

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,28 +1,44 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Arrow from "./Arrow";
 import Card from "./Card";
 import { cities } from "../cities.js";
 
 const Carousel = () => {
+  const timerRef = useRef(null);
   const [index, setIndex] = useState(0);
   const [hide, setHide] = useState(false);
   const [canScroll, setCanScroll] = useState(true);
 
   useEffect(() => {
-    setTimeout(() => {
+    //Timer to fade out the current slide
+    timerRef.hideTimer = setTimeout(() => {
       setHide(true);
-      setCanScroll(false);
+      setCanScroll(false); //Cannot use arrows while fading out
     }, 4400);
-    // setHide(true);
-    setTimeout(() => {
+    //Timer for the next slide
+    timerRef.slideTimer = setTimeout(() => {
       next();
     }, 5000);
-    // console.log(hide);
+    //Clean-up the next slide and fade out
+    return () => {
+      clearTimeout(timerRef.hideTimer);
+      clearTimeout(timerRef.slideTimer);
+    }
   }, [index]);
 
   const next = () => {
-    setCanScroll(true); //poner settimeout a 300ms
+    setHide(false); //Fade in the cards
+    setCanScroll(true); //Can use arrows again
+    if (index == cities.length - 4) {
+      setIndex(0);
+    } else {
+      setIndex(index + 4);
+    }
+  };
+
+  const nextArrow = () => {
     setHide(false);
+    setCanScroll(true);
     if (canScroll) {
       if (index == cities.length - 4) {
         setIndex(0);
@@ -34,6 +50,7 @@ const Carousel = () => {
 
   const prev = () => {
     setHide(false);
+    setCanScroll(true);
     if (canScroll) {
       if (index == 0) {
         setIndex(cities.length - 4);
@@ -43,8 +60,29 @@ const Carousel = () => {
     }
   };
 
+  const pause = () => {
+    console.log("Pausing the slides...");
+    clearTimeout(timerRef.hideTimer);
+    clearTimeout(timerRef.slideTimer);
+    setHide(false);
+    // setCanScroll(false);
+  };
+
+  const resume = () => {
+    console.log("Resuming the slides...");
+    // setCanScroll(true);
+    timerRef.hideTimer = setTimeout(() => {
+        setHide(true);
+        setCanScroll(false);
+      }, 4400);
+    timerRef.slideTimer = setTimeout(() => {
+        next();
+        // setCanScroll(true);
+      }, 5000);
+  };
+
   return (
-    <div className="flex-row">
+    <div className="flex-row" onMouseEnter={pause} onMouseLeave={resume}>
       <Arrow src="arrow-left.png" alt="arrow" fn={prev} />
       <div className="carousel">
         <Card
@@ -74,7 +112,7 @@ const Carousel = () => {
           img={cities[index + 3].img}
         />
       </div>
-      <Arrow src="arrow-right.png" alt="arrow" fn={next} />
+      <Arrow src="arrow-right.png" alt="arrow" fn={nextArrow} />
       {/* <h3>{index}</h3> */}
     </div>
   );
