5794e2fe0eceae8b8f09811256de4f34c0e6ce92
aletondaX <gtrrsta@outlook.com>
2023-08-22 23:06:59 -0300

    Added responsive navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,26 @@
+import { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 
 export default function NavBar() {
+  const [MenuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav>
-      <NavLink className={({ isActive, isPending }) =>
-        isPending ? "pending" : isActive ? "active" : ""}
-        to="/">Home</NavLink>
-      <NavLink className={({ isActive, isPending }) =>
-        isPending ? "pending" : isActive ? "active" : ""}
-        to="/cities">Cities</NavLink>
-      <Link className="highlight-button" to="#">Log In</Link>
-      <img src="def-user.png" alt="" />
+      <div className={MenuOpen ? "links-open" : "links-closed"}>
+        <NavLink className={({ isActive, isPending }) =>
+          isPending ? "pending" : isActive ? "active" : ""}
+          to="/">Home</NavLink>
+        <NavLink className={({ isActive, isPending }) =>
+          isPending ? "pending" : isActive ? "active" : ""}
+          to="/cities">Cities</NavLink>
+        <div className="login">
+          <Link className="highlight-button" to="#">Log In</Link>
+          <img src="def-user.png" alt="" />
+        </div>
+      </div>
+      <button onClick={() => setMenuOpen(!MenuOpen)} className="menu-icon">
+      {MenuOpen ? <i className="fas fa-times"></i> :
+      <i className="fas fa-bars"></i>}</button>
     </nav>
   );
 }
